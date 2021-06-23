92a6df4d80dd126a5189b08845db571ff644aa43
Anas Ansari <anas_ansar1@outlook.com>
2021-06-23 10:13:49 +0100

    fixed news list useEffect

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,8 +1,8 @@
-import useEffectForCryptoList from "../hooks/useEffectForCryptoList";
+import useEffectForStatusUpdates from "../hooks/useEffectForStatusUpdates";
 import NewsCard from "./NewsCard";
 
 export default function NewsFeed() {  
-  const [newsList, setNewsList] = useEffectForCryptoList()
+  const [newsList, setNewsList] = useEffectForStatusUpdates()
 
   return (
     <ul className="newsfeed">
